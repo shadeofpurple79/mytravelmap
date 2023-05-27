79118ece2467a3660204042eb19c7a7d4baee8c9
shadeofpurple79 <aomoss@yahoo.com>
2023-05-27 22:37:28 +0000

    Fix errors in the two urls.py files, render index

--- mytravelapp/urls.py
+++ mytravelapp/urls.py
@@ -1,9 +1,10 @@
-from . import views
 from django.urls import path
+from mytravelapp import views
+from . import views
 
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     # path('world-map/', views.world_map, name='world_map'),
 ]
