e53deb7f457306d2ed17b8810513981cbd915be7
shadeofpurple79 <aomoss@yahoo.com>
2023-05-27 20:17:21 +0000

    Create two models, destination and food

--- mytravelapp/models.py
+++ mytravelapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Destination(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city_name = models.CharField(max_length=100)
+    country_name = models.CharField(max_length=100)
+    month_year_visited = models.DateField(null=True, blank=True)
+    month_year_to_visit = models.DataField(null=True, blank=True)
+
+    EXPERIENCE_OPTIONS = (
+        ('visited', 'I have visited'),
+        ('want_to_visit', 'I want to visit'),
+    )
+    experience_option = models.CharField(
+        max_length=50, choices=EXPERIENCE_OPTIONS, default='visited'
+    )
+
+class Food(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    image = CloudinaryField('image')
+
+    def_str_(self):
+        return self.name
