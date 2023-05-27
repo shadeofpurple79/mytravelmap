711c6063ebc646b9b6514c96a90b1ea73ccd7f87
shadeofpurple79 <aomoss@yahoo.com>
2023-05-27 21:38:25 +0000

    Create urls.py under mytravelapp

---
+++ mytravelapp/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('world-map/', views.world_map, name='world_map'),
+]
