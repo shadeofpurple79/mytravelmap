097ed10e62d4547b439b03f30842273c65b281d4
shadeofpurple79 <aomoss@yahoo.com>
2023-05-30 09:46:05 +0000

    Delete duplicate app and move templates folder

--- mytravelapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Destination
-
-class DestinationForm(forms.ModelForm):
-    class Meta:
-        model = Destination
-        fields = '__all__'
-        # fields = ['city_name', 'country_name', 'month_year_visited', 'month_year_to_visit', 'experience_option']
