f7a7e4de51507812b6d1472e4aca064af197b5bb
shadeofpurple79 <aomoss@yahoo.com>
2023-06-04 14:57:16 +0000

    Force user to be logged in when adding destination

--- maps/views.py
+++ maps/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Destination
 from .forms import DestinationForm
 from django.contrib.auth.models import User
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -28,18 +28,23 @@ def index(request):
 # @login_required
 def add_destination(request):
     if request.method == 'POST':
-        user = request.user
+        # user = request.user
         form = DestinationForm(request.POST)
         if form.is_valid():
             destination = form.save(commit=False)
-            destination.user = request.user
+            destination.user = request.user # Assign the actual user
             destination.save()
-    return redirect('destination_list')
+            return redirect('destination_list')
+    else:
+        form = DestinationForm()
+    return render(request, 'add_destination.html', {'form': form})
+    
 
 def destination_list(request):
     destinations = Destination.objects.all()  # Retrieve all destinations from the database
     context = {'destinations': destinations}
-    return render(request, 'destination_list.html', context)
+    # return render(request, 'destination_list.html', context)
+    return render(request, 'destination_list.html', {'destinations': destinations})
     
 def world_map(request):
     return render(request, 'world_map.html')
