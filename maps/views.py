f00c746121240d5ac2c08fb04082a20ae1e16911
shadeofpurple79 <aomoss@yahoo.com>
2023-06-12 10:35:47 +0000

    Fix to capture map coordinates in the form

--- maps/views.py
+++ maps/views.py
@@ -10,18 +10,32 @@ from django.contrib import messages
 def index(request):
     # return render(request, 'index.html')
     if request.method == 'POST':
-        form = DestinationForm(request.POST)
+        form = DestinationForm(data=request.POST)
+        print('REQUEST METHOD IS POST')
         if form.is_valid():
+            print('IN IF BLOCK')
             # destination = form.save(commit=False)
             if request.user.is_authenticated:
-                form.instance.user = request.user  # Set the user to the logged-in user
-                form.save()
+                # form.instance.user = request.user  # Set the user to the logged-in user
+                # form.save()
+                destination = form.save(commit=False)
+                destination.user = request.user # Assign the logged in user
+                print('USER: ', destination.user)
+                destination.latitude = request.POST.get('latitude', False)
+                destination.longitude = request.POST.get('longitude', False)
+                print(f"Latitude: {destination.latitude}")
+                print(f"Longitude: {destination.longitude}")
+                destination.save()
+                print('FORM: ', destination)
                 messages.success(request, 'New destination added')
                 return redirect('index')
             else:
                 messages.warning(request, 'Please login')
                 return redirect('accounts/login')  # Redirect to the login page if the user is not authenticated
+        else:
+            print('error')
     else:
+        print('IN ELSE BLOCK')
         form = DestinationForm()
     return render(request, 'index.html', {'form': form})
     
@@ -41,18 +55,23 @@ def add_destination(request):
     if request.method == 'POST':
         # user = request.user
         form = DestinationForm(request.POST)
+        print('REQUEST METHOD IS POST')
         if form.is_valid():
+            print('IN IF BLOCK')
             destination = form.save(commit=False)
             destination.user = request.user # Assign the logged in user
-            destination.latitude = form.cleaned_data['latitude']
-            destination.longitude = form.cleaned_data['longitude']
-            print(f"Latitude: {latitude}")
-            print(f"Longitude: {longitude}")
+            print('USER: ', destination.user)
+            destination.intance.latitude = form.cleaned_data['latitude']
+            destination.intance.longitude = form.cleaned_data['longitude']
+            print(f"Latitude: {destination.intance.latitude}")
+            print(f"Longitude: {destination.intance.longitude}")
+            print('FORM: ', destination)
             destination.save()
             messages.success(request, 'New destination added')
             form = DestinationForm()
             return redirect('index')
     else:
+        print('IN ELSE BLOCK')
         form = DestinationForm()
 
     # return render(request, 'add_destination.html', {'form': form})
